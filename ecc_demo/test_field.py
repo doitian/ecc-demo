c2d63aaaad08eccfc6c196fa7bc926ef56f7e1ea
ian <me@iany.me>
2024-03-31 15:56:20 +0800

    :white_check_mark: Rewrite field test using sympy

--- ecc_demo/test_field.py
+++ ecc_demo/test_field.py
@@ -1,33 +1,28 @@
 from unittest import TestCase
 import random
-from py_ecc.fields import FQ
+import sympy
 from .field import GF
-
-
-class ReferenceGF53(FQ):
-    degree = 1
-    field_modulus = 53
 
 
 MAX = 500
 
 GF53 = GF(53)
+RGF53 = sympy.GF(53, symmetric=False)
 
 
 class FieldTest(TestCase):
     def test_random_cases(self):
-        pass
         test_data = [
             (random.randint(0, MAX), random.randint(0, MAX)) for _ in range(10)
         ]
         for a, b in test_data:
             a = GF53(a)
             b = GF53(b)
-            self.assertEqual((a + b).n, (ReferenceGF53(a.n) + ReferenceGF53(b.n)).n)
-            self.assertEqual((a - b).n, (ReferenceGF53(a.n) - ReferenceGF53(b.n)).n)
-            self.assertEqual((-a).n, (-ReferenceGF53(a.n)).n)
-            self.assertEqual((a * b).n, (ReferenceGF53(a.n) * ReferenceGF53(b.n)).n)
+            self.assertEqual((a + b).n, (RGF53(a.n) + RGF53(b.n)).val)
+            self.assertEqual((a - b).n, (RGF53(a.n) - RGF53(b.n)).val)
+            self.assertEqual((-a).n, (-RGF53(a.n)).val)
+            self.assertEqual((a * b).n, (RGF53(a.n) * RGF53(b.n)).val)
             if b.n != 0:
-                self.assertEqual((a / b).n, (ReferenceGF53(a.n) / ReferenceGF53(b.n)).n)
+                self.assertEqual((a / b).n, (RGF53(a.n) / RGF53(b.n)).val)
             if a.n != 0:
-                self.assertEqual((b / a).n, (ReferenceGF53(b.n) / ReferenceGF53(a.n)).n)
+                self.assertEqual((b / a).n, (RGF53(b.n) / RGF53(a.n)).val)
